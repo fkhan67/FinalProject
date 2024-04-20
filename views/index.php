75c142fd08e52caf850e44449b658396e79d3ce3
fkhan67 <157556820+fkhan67@users.noreply.github.com>
2024-04-20 11:28:14 -0400

    Update index.php

--- views/index.php
+++ views/index.php
@@ -1,1 +1,14 @@
+<?php
+// Include database configuration
+require_once('../config/database.php');
 
+// Include controllers
+require_once('../controllers/TeamController.php');
+require_once('../controllers/PlayerController.php');
+
+// Create instances of controllers
+$teamController = new TeamController($db);
+$playerController = new PlayerController($db);
+
+// Handle requests based on routing (not shown here)
+?>

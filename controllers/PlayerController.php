700a6d824ba4c692a0b2a8d84d1e804280734dde
fkhan67 <157556820+fkhan67@users.noreply.github.com>
2024-04-22 10:46:50 -0400

    Update PlayerController.php

--- controllers/PlayerController.php
+++ controllers/PlayerController.php
@@ -1,13 +1,21 @@
 <?php
 require_once('../models/Player.php');
-
 class PlayerController {
     private $playerModel;
-
     public function __construct($db) {
         $this->playerModel = new Player($db);
     }
-
-    // Methods to handle player-related requests (e.g., listing, creating, editing players)
+    public function listPlayers() {
+        // Fetch players from the database using the Player model
+    }
+    public function createPlayer($data) {
+        // Create a new player using the Player model
+    }
+    public function updatePlayer($id, $data) {
+        // Update an existing player using the Player model
+    }
+    public function deletePlayer($id) {
+        // Delete a player using the Player model
+    }
 }
 ?>

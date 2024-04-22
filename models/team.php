b8cd357805aff7f9d0144bff908f56144c902651
fkhan67 <157556820+fkhan67@users.noreply.github.com>
2024-04-22 10:54:19 -0400

    Update team.php

--- models/team.php
+++ models/team.php
@@ -1,10 +1,8 @@
 class Team {
     private $db;
-
     public function __construct($db) {
         $this->db = $db;
     }
-
     // Create a new team
     public function create($name) {
         $query = "INSERT INTO teams (name) VALUES (:name)";
@@ -12,14 +10,12 @@ class Team {
         $stmt->bindParam(':name', $name);
         return $stmt->execute();
     }
-
     // Read all teams
     public function getAllTeams() {
         $query = "SELECT * FROM teams";
         $stmt = $this->db->query($query);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
     // Read a team by ID
     public function getTeamById($id) {
         $query = "SELECT * FROM teams WHERE id = :id";
@@ -28,7 +24,6 @@ class Team {
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
-
     // Update a team
     public function update($id, $name) {
         $query = "UPDATE teams SET name = :name WHERE id = :id";
@@ -37,7 +32,6 @@ class Team {
         $stmt->bindParam(':id', $id);
         return $stmt->execute();
     }
-
     // Delete a team
     public function delete($id) {
         $query = "DELETE FROM teams WHERE id = :id";
